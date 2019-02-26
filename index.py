f5ed1856167d422dea060e9357e34e5532a21b47
Oliver Plunkett <otplunkett@gmail.com>
2019-02-26 14:03:18 -0500

    yes

--- index.py
+++ index.py
@@ -92,5 +92,6 @@ if __name__ == '__main__':
     if args.seed:
         seed_db(args.lang)
     else:
+        seed_db()
         port = int(os.environ.get("PORT", 5000))
         app.run(host='0.0.0.0', debug=False, port=port)
